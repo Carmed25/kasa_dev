685d8a666c889a43ecd2df17739dd9414d74738c
Carmedine <carmedine@protonmail.com>
2025-08-24 10:54:04 +0200

    initialisation du projet avec React  et React Router

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+// import data from './data/appartements.json'
+import './App.css'
+
+function App() {
+  // state(etat, donnees, data)
+
+  // comportement
+
+  // affichage (render)
+
+  return (
+    <div>
+      <h1>Page d'accueil</h1>
+      <p>Tout fonctionne pour le moment</p>
+    </div>
+  )
+}
+
+export default App
