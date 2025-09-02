035015f6ba127d0737e803c86a5d28d2a4a5a5de
Carmedine <carmedine@protonmail.com>
2025-09-02 06:10:32 +0200

    page about à revoir pour les chevrons et le  responsive

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,13 +1,37 @@
 
 import './Collapse.scss'
 import arrowTop from '../../assets/arrowTop.svg'
+import { Children, useState } from 'react';
 
 // clic sur chevron ouvre une fenetre 
-export default function Collapse(){
+export default function Collapse({title,children}){
+    //state ( données - état):état initial fermé
+    const [isOpen, setIsOpen]=useState(false);
+
+
+
+    //comportements: quand clic , on inverse le résultat initial
+    const handleClick=()=>{
+        setIsOpen(!isOpen);
+    
+    }
+
+    const chevronClass=`collapse-header_logo ${isOpen?'rotate':''}`
+
+    //affichage ( render )
     return (
-        <div className='collapse-container'>
-            <h1 className='collapse-container_title'>Title</h1>
-             <img className='collapse-container_logo' src={arrowTop} alt='Chevron'/>
+        <div className='collapse'>
+            <div className='collapse-header'>
+                <h1 className='collapse-header_title'>{title}</h1>
+                <button onClick={handleClick}>
+                    <img className={chevronClass} src={arrowTop} alt='Chevron'/></button>
+
+             
+            </div>
+
+            {isOpen &&(
+                <div className='collapse_content'>{children}</div>
+            )}
         </div>
     )
 }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,19 +1,26 @@
-//import Footer  from '../../components/Footer/Footer'
-//import Header from '../../components/Header/Header'
+
 import Banniere from '../../components/Banniere/Banniere'
 import './About.scss'
 import imgBanAbout from '../../assets/imgBanniereAPropos.jpg'
 import Collapse from '../../components/Collapse/Collapse'
+
 export default function About(){
     return(
         <div>
             <Banniere
             image={imgBanAbout} opacity={0.3}/>
+
             <div className='bodyAbout'>
-                <Collapse/>
-                <Collapse/>
-                <Collapse/>
-                <Collapse/>
+                <Collapse title="Fiabilité" >
+                <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
+                </Collapse>
+                <Collapse title="Respect" >
+                <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+                </Collapse><Collapse title="Service" >
+                <p>La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
+                </Collapse><Collapse title="Sécurité" >
+                <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+                </Collapse>
             </div>
         </div>
     )
